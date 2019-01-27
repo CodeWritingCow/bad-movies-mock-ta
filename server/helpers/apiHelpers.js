155cef9ca1ed224bc1486f8bbffe386bb8e92d27
Gary Pang <gary.pang@gmail.com>
2019-01-27 16:22:20 -0500

    Change search parameter in getMoviesByGenre
    getMoviesByGenre searches for movies by least number of votes
    instead of popularity.

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -25,7 +25,7 @@ let getGenreList = function () {
 }
 
 let getMoviesByGenre = function(genreId) {
-    return axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.asc&with_genres=${genreId}`)
+    return axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=vote_average.asc&with_genres=${genreId}`)
     .then(function(res) {
         console.log(res.data.results);
         return res.data.results;
