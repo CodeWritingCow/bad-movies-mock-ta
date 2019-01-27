6624eab40a3c4912e7af3fde6ed8590e4884d7b9
Gary Pang <gary.pang@gmail.com>
2019-01-27 12:12:47 -0500

    Write API helper function getGenreList

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -10,3 +10,19 @@ const { API_KEY } = require('../../config.js');
 // Get your API Key and save it in your config file
 
 // Don't forget to export your functions and require them within your server file
+
+// get official list of genres from themoviedb
+let getGenreList = function () {
+    axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
+      .then(function(res) {
+          return res.data.genres;
+      })
+      .catch(function (err) {
+          console.log(err);
+          return err;
+      });
+}
+
+module.exports = {
+    getGenreList: getGenreList
+};
