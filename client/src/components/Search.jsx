40443401068fb683619475fdf0e62af20b3be07d
Gary Pang <gary.pang@gmail.com>
2019-01-27 16:07:55 -0500

    Implement dynamic select genres droplist

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      genres: []
+      genres: [1,2,3]
     };
+    this.getGenres = this.getGenres.bind(this);
   }
+
+
   getGenres() {
     //make an axios request in this component to get the list of genres from your endpoint GET GENRES
+    axios.get('/genres')
+      .then((res) => {
+        let result = res.data.map((genre) => {
+          return genre.name;
+        });
+        // console.log(result);
+      this.setState({ genres: result })
+    }).catch((error) => {
+      console.log(error);
+    });
   }
 
   render() {
+    // console.log(this.props);
     return (
       <div className="search">
         <button onClick={() => {this.props.swapFavorites()}}>{this.props.showFaves ? "Show Results" : "Show Favorites"}</button>
@@ -20,10 +35,11 @@ class Search extends React.Component {
         {/* Make the select options dynamic from genres !!! */}
         {/* How can you tell which option has been selected from here? */}
 
-        <select>
-          <option value="theway">The Way</option>
-          <option value="thisway">This Way</option>
-          <option value="thatway">That Way</option>
+        <select onClick={()=> {this.getGenres()}}>
+        {/* <select> */}
+          {/* { this.props.genres.map((genre) => <option value={genre}>{genre}</option>) } */}
+          {/* { [1,2,3].map((num) => <option value={num}>{num}</option>) } */}
+          { this.state.genres.map((genre) => <option value={genre}>{genre}</option>) }
         </select>
         <br/><br/>
 
