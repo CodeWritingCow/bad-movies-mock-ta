b7b437131d90c7ce719ccb9cf4c53993bfc1549c
Gary Pang <gary.pang@gmail.com>
2019-02-03 21:06:03 -0500

    Create SQL schema for favorite films

--- db/sql/moviesSchema.sql
+++ db/sql/moviesSchema.sql
@@ -1,1 +1,19 @@
 -- SET UP SCHEMA HERE
+
+CREATE DATABASE favorites;
+
+USE favorites;
+
+CREATE TABLE movies (
+  id int,
+  title VARCHAR(255) NOT NULL,
+  poster_path VARCHAR(255) NOT NULL,
+  release_date VARCHAR(255) NOT NULL,
+  popularity INT NOT NULL,
+  PRIMARY KEY (id)
+--   genre_ids: Array
+);
+
+/*  Execute this file from the command line by typing:
+ *    mysql -u root < server/schema.sql
+ *  to create the database and the tables.*/
